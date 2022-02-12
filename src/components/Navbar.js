daf495885225f2ef561683dd77aaed6458ab9a97
Baranga69 <69077312+Baranga69@users.noreply.github.com>
2022-02-12 11:57:56 +0300

    Starting on React
    This was my kickstarting of my Js journey on React, more incoming

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+  return (
+      <>
+      <nav className = "navbar">
+         <div className="navbar-container">
+             <Link to="/" className="navbar-logo">
+                 TRVL
+             </Link>
+             <div class Name='menu-icon'>
+                 
+
+             </div>
+         </div>
+      </nav>
+      </>
+  )
+}
+
+export default Navbar
